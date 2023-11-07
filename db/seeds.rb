6a7a0752c3c24312156371accacbf9f9ef62a77b
Pablo Peuscovich <ppeusco@gmail.com>
2023-11-07 17:35:06 -0300

    Seeding the database with test data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,8 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+
+user = User.create(name: "John Doe", email: "john@example.com")
+
+post1 = user.posts.create(title: "First post", body: "This is the first post.")
+post2 = user.posts.create(title: "Second post", body: "This is the second post.")
+
+post1.comments.create(body: "This is a comment on the first post.", user: user)
+post2.comments.create(body: "This is a comment on the second post.", user: user)
